5214cd86c2c4c61370caa8e581e5b6885f7a41d4
tenglongroy <379600075@qq.com>
2021-06-22 01:57:54 +1000

    add algorithm and explanation.

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace everlight_technical_challenge
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0){
+                int depth = Convert.ToInt32(args[0]);
+                BallTree test = new BallTree(depth);
+            }
+        }
+    }
+    class BallTree
+    {
+        List<int> heap;
+        Random random;
+        public BallTree(int depth)
+        {
+
+            this.heap = new List<int>();
+            this.heap.Add(0);
+            this.random = new Random((int)DateTime.Now.Ticks);
+            this.generateTree(this.heap, depth);
+            int predictContainer = this.predictNoBall(this.heap, depth);
+            this.printTreeStructure(this.heap, depth);
+            Console.WriteLine("The predicted container is [" + (char)('A' + predictContainer) + "]\n");
+            Console.WriteLine("----------------");
+            this.traverseTree(this.heap, depth);
+            this.checkEmptyContainer(this.heap, depth);
+        }
+
+        void generateTree(List<int> heapList, int depth){
+            int totalLength = (int)Math.Pow(2, depth);
+            // store gateSide into heapList
+            for(int ind = 1; ind < totalLength; ind ++){
+                int gateSide = this.random.Next(-1, 1) < 0 ? -1 : 1;    // if -1 means the gate is opening left, 1 means opening right
+                heapList.Add(gateSide);
+            }
+            // the tree leaves store container name into as 1, 2, 3, 4.... reflecting A, B, C, D....
+            for(int ind = 1; ind <= totalLength; ind ++){
+                heapList.Add(ind);
+            }
+        }
+
+        void printTreeStructure(List<int> heapList, int depth){
+            Console.WriteLine("The heap array for gate side is:");
+            for(int ind = 1; ind < heapList.Count; ind++){
+                /* Console.Write(heapList[ind]); */
+                if (ind < Math.Pow(2, depth)){  // gate side
+                    Console.Write(heapList[ind]);
+                }
+                else{   // container name
+                    Console.Write((char)(heapList[ind]-1 + 'A'));
+                }
+                
+                Console.Write(" ");
+            }
+            Console.WriteLine("");
+        }
+
+        int predictNoBall(List<int> heapList, int depth){
+            int nextInd = 1, currentGateSide = 0;
+            while(Math.Pow(2, depth) > nextInd){    // keep looping as long as nextInd not over gate index
+                currentGateSide = heapList[nextInd];
+                nextInd = nextInd * 2 + (currentGateSide < 0 ? 1 : 0);    // if gateSide opening to left, go find right sub-tree; vice versa
+            }
+            int containerInd = nextInd - (int)Math.Pow(2, depth);
+            return containerInd;
+        }
+
+        void traverseTree(List<int> heapList, int depth){
+            int totalBall = (int)Math.Pow(2, depth) - 1;
+            for (int ind = 0; ind < totalBall; ind ++){
+                this._traverseTree(heapList, depth, ind);
+            }
+        }
+        void _traverseTree(List<int> heapList, int depth, int ballInd){
+            List<int> path = new List<int>();
+            int nextInd = 1, currentGateSide = 0;
+            while(Math.Pow(2, depth) > nextInd){
+                path.Add(nextInd);
+                currentGateSide = heapList[nextInd];
+                heapList[nextInd] *= -1;
+                nextInd = nextInd * 2 + (currentGateSide < 0 ? 0 : 1);
+            }
+            heapList[nextInd] = -99;
+            int containerInd = nextInd - (int)Math.Pow(2, depth);
+            char containerName = (char)('A' + containerInd);
+
+            Console.Write("Path for No." + ballInd + " ball -> ");
+            Console.WriteLine(string.Join(" ", path));
+
+            Console.WriteLine("Reach container : " + containerName);
+
+            Console.Write("Current Gate direction ==>> ");
+            Console.WriteLine(string.Join(" ", heapList.GetRange(1, heapList.Count-1)));
+            
+            Console.WriteLine("----------------");
+        }
+
+        void checkEmptyContainer(List<int> heapList, int depth){
+            int containerStartInd = (int)Math.Pow(2, depth);
+            for(; containerStartInd < heapList.Count; containerStartInd ++){
+                if (heapList[containerStartInd] != -99){
+                    Console.WriteLine("\nEmpty container is [" + (char)('A' + containerStartInd - (int)Math.Pow(2, depth)) + "]");
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # everlight-technical-challenge
 Everlight Radiology technical challenge
+
+## How to run
+Simply type "dotnet run [depth]" to run and test the program. The program will write the required information to console.
+
+## Data structure explain:
+The program uses a heap array to represent a tree. The root node start at position 1 of the heap array, and the left child node of each node is 2*i, and the right chid node is 2*i + 1.
+
+After storing all the gate directions, the heap array also store the containers' index, and print it out as ASCII characters.
+
+### Prediction algorithm explain:
+The predicted empty container is the container whose ancesters all have gates in opposite direction.
+
+If depth is 1 and a node's gate is facing itself, it only need once to get a ball. Otherwise the node will need two times to get a ball.
+
+Which means if depth is 2, worst case is 4 times, and depth 3 will be 8 times. However, depth 3 has only 7 balls, so all we need is to find the container that has all the gates facing opposite direction.
+
+### Run balls explain:
+Each time a ball finds a container, the container index in the heap array will be changed to -99. After all balls done, the only container that's not -99 will be the empty one.
+
+You can compare the last WriteLine with the prediction at beginning.
